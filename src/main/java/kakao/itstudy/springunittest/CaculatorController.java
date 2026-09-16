7b922969b71fabcee739d6c81a369359a1c3f064
itstudy <itstudy.kakao.com>
2026-09-16 17:38:07 +0900

    sonarqube

--- src/main/java/kakao/itstudy/springunittest/CaculatorController.java
+++ src/main/java/kakao/itstudy/springunittest/CaculatorController.java
@@ -1,6 +1,8 @@
 package kakao.itstudy.springunittest;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CaculatorController {
     private final Calculator calculator;
+    private final Logger logger = LoggerFactory.getLogger(CaculatorController.class);
+
     @RequestMapping("/")
     String index(){
+        logger.debug(10+20 + "");
         return "OK";
     }
     @RequestMapping("/health")
